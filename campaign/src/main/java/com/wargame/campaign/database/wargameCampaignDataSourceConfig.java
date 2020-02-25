3384916e7e12c8848a2661f4928a061dc6e01dfa
John Polhill <johnpolhill42@gmail.com>
2020-02-24 20:30:07 -0600

    Start database

---
+++ campaign/src/main/java/com/wargame/campaign/database/wargameCampaignDataSourceConfig.java
@@ -0,0 +1,31 @@
+package com.wargame.campaign.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * 2/24/20にjohnpolhilによって作成されました。
+ */
+@Configuration
+@ConfigurationProperties(prefix = "wargamecampaign.db")
+public class wargameCampaignDataSourceConfig extends HikariConfig {
+
+    @Bean(destroyMethod = "close", name = "wargame_campaign_data_source")
+    DataSource pulseDataSource() {
+        return new HikariDataSource(this);
+    }
+
+    @Bean(name = "wargameCampaignJdbcTemplate")
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(@Qualifier("wargame_campaign_data_source") DataSource pulseDataSource) {
+        return new NamedParameterJdbcTemplate(new JdbcTemplate(pulseDataSource));
+    }
+
+}
